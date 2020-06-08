f8456435ca0627f947a2f0d65945be5fdef671f9
Laura I <lsilves@myseneca.ca>
2020-06-07 23:56:13 -0400

    fixing up about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,7 +12,40 @@ export class AboutComponent implements OnInit {
 
   constructor() { }
 
+
+  scrollToBottom(){
+    if(this.who && this.how && this.interests){
+      console.log("THIS IS EVENT.TARGET -> ", (event.target as Element).id);
+      document.getElementById("#interestPar").animate({scrollTop:document.getElementById("#interestPar").scrollHeight}, 1000);
+    }
+
+  }
+
   ngOnInit(): void {
+  }
+
+  openPar(par){
+    switch(par){
+      case 'how' :{
+        this.how = !this.how;
+        this.who = false;
+        this.interests = false;
+      }
+      break;
+      case 'who' :{
+        console.log("who?")
+        this.who = !this.who;
+        this.how = false;
+        this.interests = false;
+      }
+      break;
+      case 'interests' :{
+        this.interests = !this.interests;
+        this.who = false;
+        this.how = false;
+      }
+      break;
+    }
   }
 
 }
