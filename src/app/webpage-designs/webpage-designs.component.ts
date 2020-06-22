4b6423dafe35d1be6d4e49f029cf1fbc3ba35bc2
Laura I <lsilves@myseneca.ca>
2020-06-21 22:12:08 -0400

    nav update, webdesigns update

--- src/app/webpage-designs/webpage-designs.component.ts
+++ src/app/webpage-designs/webpage-designs.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebpageDesignsComponent implements OnInit {
 
+  workTitle  = true;
+  workContent = false; 
   curSlide = 0;
   graphics = [
     {
